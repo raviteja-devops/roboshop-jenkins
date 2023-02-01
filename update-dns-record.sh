5543cfb6e6edf3daed49addccf98bb91f8a4189c
raviteja <ravitejatfcdemo@gmail.com>
2023-02-01 11:36:08 +0530

    adding Jenkinsfile

---
+++ update-dns-record.sh
@@ -0,0 +1,17 @@
+IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=jenkins"  --query 'Reservations[*].Instances[*].PublicIpAddress' --output text)
+
+echo '
+{
+  "Comment": "CREATE/DELETE/UPSERT a record ",
+  "Changes": [{
+    "Action": "UPSERT",
+    "ResourceRecordSet": {
+      "Name": "jenkins.raviteja.online",
+      "Type": "A",
+      "TTL": 15,
+      "ResourceRecords": [{ "Value": "IPADDRESS"}]
+    }}]
+}' | sed -e "s/IPADDRESS/${IP}/" >/tmp/jenkins.json
+
+ZONE_ID="Z040944033CUGWUDK9I6T"
+aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/jenkins.json | jq .
